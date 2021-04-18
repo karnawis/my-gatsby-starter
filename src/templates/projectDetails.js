6424e3b576ec9946f289439ce518a1ac63c9a49b
Sura <sk@sura.me>
2021-04-18 19:58:50 -0400

    details-page-query-variable-added

--- src/templates/projectDetails.js
+++ src/templates/projectDetails.js
@@ -1,43 +1,44 @@
-import React from "react"
+import { graphql } from 'gatsby'
+import React from 'react'
 import Img from "gatsby-image"
-import Layout from "../components/Layout"
-import styles from '../styles/project-details.module.css'
-import { graphql } from "gatsby"
+import Layout from '../components/Layout'
+import * as styles from '../styles/project-details.module.css'
 
-const ProjectDetails = ({ data }) => {
+
+export default function ProjectDetails( { data }) {
+  console.log(data)
   const { html } = data.markdownRemark
   const { title, stack, featuredImg } = data.markdownRemark.frontmatter
 
   return (
     <Layout>
-      <div>
-        <h2>{title}</h2>
+      <div className="details">
+        <h1>{title}</h1>
         <h3>{stack}</h3>
-        <div>
+        <div className={styles.featured}>
           <Img fluid={featuredImg.childImageSharp.fluid} />
         </div>
+        <div className={styles.html} dangerouslySetInnerHTML={{__html: html}}/>
       </div>
     </Layout>
   )
 }
- 
-export default ProjectDetails
 
 export const query = graphql`
-  query ProjectDetails($slug: String) {
-    markdownRemark(frontmatter: {slug: {eq: $slug}}) {
-      html
-      frontmatter {
-        stack
-        title
-        featuredImg {
-          childImageSharp {
-            fluid {
-              ...GatsbyImageSharpFluid
-            }
+query ProjectDetails($slug: String) {
+  markdownRemark(frontmatter: {slug: {eq: $slug}}) {
+    html
+    frontmatter {
+      stack
+      title
+      featuredImg {
+        childImageSharp {
+          fluid {
+            ...GatsbyImageSharpFluid
           }
         }
       }
     }
   }
+}
 `
