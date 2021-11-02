55e6d9d6957621e1b96de217958d7a5f39f06df7
Sura <sk@sura.me>
2021-11-01 20:19:08 -0400

    Added social media component

---
+++ src/components/SocialMedia.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export default function SocialMedia() {
+    return (
+    <div class="col col-12 align-self-center">
+        <ul>
+            <li><a href="https://www.facebook.com/followyourheartart/" target="_blank"><i class="fa fa-facebook" aria-hidden="true"></i>FACEBOOK</a></li>
+            <li><a href="https://plus.google.com/u/0/107854673355077388333/" target="_blank"><i class="fa fa-google-plus" aria-hidden="true"></i>GOOGLE</a></li>
+            <li><a href="https://twitter.com/#!/sura_kittie/" target="_blank"><i class="fa fa-twitter" aria-hidden="true"></i>TWITTER</a></li>
+            <li><a href="http://www.pinterest.com/skarnawi/" target="_blank"><i class="fa fa-pinterest-p" aria-hidden="true"></i>PINTREST</a></li>
+            <li><a href="http://instagram.com/surakittie" target="_blank"><i class="fa fa-instagram" aria-hidden="true"></i>INSTAGRAM</a></li>
+            <li><a href="http://ca.linkedin.com/in/surait/" target="_blank"><i class="fa fa-linkedin" aria-hidden="true"></i>LINKEDIN</a></li>
+        </ul>
+    </div>
+    )
+}
