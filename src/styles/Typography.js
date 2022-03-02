6f0a238fcc79c7202057958f11333032a82a9664
Sura <sk@sura.me>
2022-03-02 13:48:30 -0500

    Added bold typography

--- src/styles/Typography.js
+++ src/styles/Typography.js
@@ -1,6 +1,8 @@
 import { createGlobalStyle } from 'styled-components'
 import fontMontserrat from '../assets/fonts/Montserrat-VariableFont_wght.ttf'
 import fontMontserratBackup from '../assets/fonts/Montserrat-VariableFont_wght.woff'
+import fontMontserratBold from '../assets/fonts/Montserrat-ExtraBold.ttf'
+import fontMontserratBoldBackup from '../assets/fonts/Montserrat-ExtraBold.woff'
 /*
 //font swap so you have a fall back 
 // fluid type : an expression of how big the font is rather than strict
@@ -10,11 +12,23 @@ import fontMontserratBackup from '../assets/fonts/Montserrat-VariableFont_wght.w
 const Typography = createGlobalStyle`
   @font-face {
     font-family: 'Montserrat';
-    src: url(${fontMontserrat}), url(${fontMontserratBackup});
+    src: url(${fontMontserrat}) format('truetype'),
+         url(${fontMontserratBackup}) format('woff');
   }
+
+  @font-face {
+    font-family: 'Montserrat-Bold';
+    src:url(${fontMontserratBold}) format('truetype'),
+        url(${fontMontserratBoldBackup}) format('woff');
+  }
+
   html {
     font-family: 'Montserrat', sans-serif;
 
+  }
+
+  h1, h2, h3, h4, h5, h6 {
+      font-family:  'Montserrat-Bold'
   }
   `
 export default Typography
